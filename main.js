cd41eccf0ad98757209359106c658f5a679b11ff
Bruno Nishimura <by.nishimura@gmail.com>
2020-11-26 12:11:42 -0300

    Correçao do Countdown e Pop-up

--- main.js
+++ main.js
@@ -1,25 +1,56 @@
+let alertaExibido = false
 // Countdown
 let dataAtual = document.querySelector('.data-atual')
 let dataEvento = document.querySelector('.data-evento')
-let timer = document.querySelector('.timer')
+let timerDays = document.querySelector('.timer-days')
+let timerHours = document.querySelector('.timer-hours')
+let timerMinutes = document.querySelector('.timer-minutes')
+let timerSeconds = document.querySelector('.timer-seconds')
 
-setInterval(function () {
-    let data = new Date()
-    let diaEvento = new Date('Nov 26, 2020 18:00:00')
-    let resultadoData = Math.floor(diaEvento - data)
-    let dias = Math.floor(resultadoData / 1000 * 60 * 60 * 24)
-    let seconds = Math.floor(resultadoData % (1000 * 60) / 1000)
+let diaEvento = new Date('Nov 26, 2020 18:00:00')
 
-    dataAtual.innerHTML = data
-    dataEvento.innerHTML = resultadoData
-    timer.innerHTML = seconds 
-}, 1000)
+var myfunc = setInterval(function () {
+    let data = new Date().getTime();
+    let resultadoData = (diaEvento - data)
+    
+    let dias = Math.floor(resultadoData / (1000 * 60 * 60 * 24));
+    let horas = Math.floor(resultadoData % (1000 * 60 * 60 * 24) / (1000 * 60 *60));
+    let minutos = Math.floor(resultadoData % (1000 * 60 * 60) / (1000 * 60));    
+    let segundos = Math.floor(resultadoData % (1000 * 60) / 1000);
 
-setTimeout
+    timerDays.innerHTML = dias 
+    timerHours.innerHTML = horas 
+    timerMinutes.innerHTML = minutos 
+    timerSeconds.innerHTML = segundos 
+
+    if (resultadoData < 0) {
+        clearInterval(myfunc);
+        document.querySelector(".timer-days").innerHTML = ""
+        document.querySelector(".timer-hours").innerHTML = "" 
+        document.querySelector(".timer-minutes").innerHTML = ""
+        document.querySelector(".timer-seconds").innerHTML = ""
+        document.querySelector("end").innerHTML = "ESTAMOS AO VIVO!!";
+    }
+}, 1000);
 
 // Pop-up
 let div = document.querySelector('body')
+  
 
 div.addEventListener('mouseleave', function (){
-    alert('Não esqueça o seu eBook! Obrigado por nos visitar! ')
+        if (!alertaExibido){
+        alert('Não esqueça o seu eBook! Obrigado por nos visitar! ')
+        alertaExibido = true
+    }
 })
+
+function podcast (){
+  
+
+}
+// >
+// >=
+// <
+// <=
+// ==
+// !=
