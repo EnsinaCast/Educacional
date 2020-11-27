d3d665933771ea1e1c35dc9296cb46bbc0cfb7cc
Bruno Nishimura <by.nishimura@gmail.com>
2020-11-27 06:34:19 -0300

    Countdown & README done

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# EnsinaCast.github.io
+<h1 align="center">
+    <img alt="EnsinaCast" title="#EnsinaCast" src="./assets/img/logo.png" width="120px" />
+</h1>
+
+## Sobre o Projeto 💻
+
+O EnsinaCast nasceu quando um grupo de malucos se juntam e decidem compartilhar conhecimentos sobre diversos assuntos! Começando pelo tema: "Qual será o futuro da educação em meio a Pandemia?"  
+
+## O Começo / Diário 
+
+Logo após a reunião de Domingo (22/11), os Hipsters enviaram a primeira versão do desing da landing page.   
+
+Após a primeira madrugada a primeira versão da landing page estava no ar o mais rápido possível! Resultado:
+
+<h1 align="center">
+    <img alt="versaobugada" title="" src="./assets/img/versaobugada.png" width="400px" />
+</h1>
+
+Um desastre! Mas pelo menos a landing page já estava no ar!
+Ao final do mesmo dia, após outra reunião foi decidido usar o RD Station...
+
+Após a segunda madrugada na terça-feira de manhã já estava no ar a nova landing page! Então ela se tornou a nossa página oficial [EstudaCast](https://evento.ensinacast.com.br/futuro2021).
+
+## Checklist - Hacker 🐴 
+Para não abandonar o repositório, continuei trabalhando nele em paralelo entender um pouco mais. Aprendizado:
+- Um pouco mais de noção em HTML, CSS e JavaScript;
+- Como fazer um botão e linkar ele;
+- Como fazer um Pop-up;
+- Como não ativar o Pop-up o tempo todo;
+- Como não fazer um Countdown;
+- Aprendi a pesquisar: "[How to create a countdown timer](https://www.w3schools.com/howto/howto_js_countdown.asp)"
+- Integrar pixels de rastreamento em páginas do projeto; 
+
--- main.js
+++ main.js
@@ -1,45 +1,75 @@
-let alertaExibido = false
-// Countdown
-let dataAtual = document.querySelector('.data-atual')
-let dataEvento = document.querySelector('.data-evento')
-let timerDays = document.querySelector('.timer-days')
-let timerHours = document.querySelector('.timer-hours')
-let timerMinutes = document.querySelector('.timer-minutes')
-let timerSeconds = document.querySelector('.timer-seconds')
+// // Primeira tentativa do Countdown Fail
+// let dataAtual = document.querySelector('.data-atual')
+// let dataEvento = document.querySelector('.data-evento')
+// let timerDays = document.querySelector('.timer-days')
+// let timerHours = document.querySelector('.timer-hours')
+// let timerMinutes = document.querySelector('.timer-minutes')
+// let timerSeconds = document.querySelector('.timer-seconds')
 
-let diaEvento = new Date('Nov 27, 2020 12:00:00')
+// let diaEvento = new Date('Dez 24, 2020 00:00:00')
 
-var myfunc = setInterval(function () {
-    let data = new Date().getTime();
-    let resultadoData = (diaEvento - data)
+// var myfunc = setInterval(function () {
+//     let data = new Date().getTime();
+//     let resultadoData = (diaEvento - data)
     
-    let dias = Math.floor(resultadoData / (1000 * 60 * 60 * 24));
-    let horas = Math.floor(resultadoData % (1000 * 60 * 60 * 24) / (1000 * 60 *60));
-    let minutos = Math.floor(resultadoData % (1000 * 60 * 60) / (1000 * 60));    
-    let segundos = Math.floor(resultadoData % (1000 * 60) / 1000);
+//     let dias = Math.floor(resultadoData / (1000 * 60 * 60 * 24));
+//     let horas = Math.floor(resultadoData % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+//     let minutos = Math.floor(resultadoData % (1000 * 60 * 60) / (1000 * 60));    
+//     let segundos = Math.floor(resultadoData % (1000 * 60) / 1000);
 
-    timerDays.innerHTML = dias 
-    timerHours.innerHTML = horas 
-    timerMinutes.innerHTML = minutos 
-    timerSeconds.innerHTML = segundos 
+//     timerDays.innerHTML = dias 
+//     timerHours.innerHTML = horas 
+//     timerMinutes.innerHTML = minutos 
+//     timerSeconds.innerHTML = segundos 
 
-    if (resultadoData < 0) {
-        clearInterval(myfunc);
-        document.querySelector(".timer-days").innerHTML = ""
-        document.querySelector(".timer-hours").innerHTML = "" 
-        document.querySelector(".timer-minutes").innerHTML = ""
-        document.querySelector(".timer-seconds").innerHTML = ""
-        document.querySelector("end").innerHTML = "ESTAMOS AO VIVO!!";
-    }
-}, 1000);
+//     if (resultadoData < 0) {
+//         clearInterval(myfunc);
+//         document.querySelector(".timer-days").innerHTML = ""
+//         document.querySelector(".timer-hours").innerHTML = "" 
+//         document.querySelector(".timer-minutes").innerHTML = ""
+//         document.querySelector(".timer-seconds").innerHTML = ""
+//         document.querySelector("end").innerHTML = "Ho ho ho! É Natal";
+//     }
+// }, 1000);
 
 // Pop-up
+let alertaExibido = false
 let div = document.querySelector('body')
-  
 
 div.addEventListener('mouseleave', function (){
         if (!alertaExibido){
-        alert('Não esqueça o seu eBook! Obrigado por nos visitar! ')
+        alert('Obrigado por nos visitar!')
         alertaExibido = true
     }
 })
+
+// Segunda tentativa Countdown
+// Set the date we're counting down to
+var countDownDate = new Date("Dec 24, 2020 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "HoHoHo! Feliz Natal!";
+  }
+}, 1000);
+
